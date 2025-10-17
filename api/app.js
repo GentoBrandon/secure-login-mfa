82b872264bec4d9c6700dbc97d14606a0d8d8f25
GentoBrandon <128234033+GentoBrandon@users.noreply.github.com>
2025-10-17 00:00:55 -0600

    feat: add configuration for docker service api

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8001
 const pgp = require('pg-promise')(/* options */)
-const db = pgp('postgresql://umg:mfa-umg@localhost:5432/postgres?schema=public')
+const db = pgp('postgresql://umg:mfa-umg@db:5432/postgres?schema=public')
 
 app.get('/', (req, res) => {
   db.any('SELECT * FROM products')
@@ -18,6 +18,21 @@ app.get('/', (req, res) => {
       error: error.message
     });
   });
+});
+
+app.get('/products', async (req, res) => {
+  const products = await db.any('SELECT * FROM products');
+  try {
+    res.json({
+      success: true,
+      data: products
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message
+    });
+  }
 });
 
 app.listen(port, () => {
