987831639acfe9caf00ee5de630e4d9932becb4a
GentoBrandon <128234033+GentoBrandon@users.noreply.github.com>
2025-10-16 18:06:09 -0600

    feat: generate example in express

---
+++ api/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 8001
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgresql://umg:mfa-umg@localhost:5432/postgres?schema=public')
+
+app.get('/', (req, res) => {
+  db.any('SELECT * FROM products')
+  .then((products) => {
+    res.json({
+      success: true,
+      data: products
+    });
+  })
+  .catch((error) => {
+    res.status(500).json({
+      success: false,
+      error: error.message
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
